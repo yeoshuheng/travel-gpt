dfe85b4d49ca0c859db35f5b71e110756350c2c0
yeoshuheng <yeoshuheng@gmail.com>
2023-06-19 18:18:11 +0800

    refactor: added more template + changed to more appropriate name

---
+++ model/TemplateGenerator.py
@@ -0,0 +1,45 @@
+from langchain.prompts import PromptTemplate
+
+class TemplateGenerator:
+    def __init__(self):
+       pass
+    
+    def getTemplates(self) -> list:
+       
+        locprompt =  (PromptTemplate(
+            input_variables = ['location'],
+            template = 'Give me a description of {location}'
+        ), "A BRIEF DESCRIPTION")
+
+        hiddengems = (PromptTemplate(
+            input_variables = ["location"],
+            template = "Give me a list of hidden gems in {location} formatted nicely"
+        ), "HIDDEN GEMS")
+
+        cuisineinfo = (PromptTemplate(
+            input_variables = ["location"],
+            template = "I am tourist, give me 5 food recomendations that I must try when visiting {location}"
+        ), "CUISINE")
+
+        accomsinfo = (PromptTemplate(
+            input_variables = ["location"],
+            template = "I am tourist, give me 3 good accomodations at {location}"
+        ), "ACCOMODATIONS")
+
+        transportinfo = (PromptTemplate(
+            input_variables = ["location"],
+            template = "I am tourist, give me transport options when I am travelling at {location}"
+        ), "GETTING AROUND")
+
+
+        weatherinfo = (PromptTemplate(
+            input_variables = ["location"],
+            template = "What is the weather like at {location}"
+        ), "WEATHER")
+
+
+
+        return [locprompt, hiddengems, cuisineinfo, accomsinfo, transportinfo, weatherinfo]
+
+
+    
