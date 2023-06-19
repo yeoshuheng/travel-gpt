b39f68efd00fe178bccabbbfb87dfccb97edd346
yeoshuheng <yeoshuheng@gmail.com>
2023-06-19 18:17:47 +0800

    feat: added multi-template handling

--- model/chatBot.py
+++ model/chatBot.py
@@ -3,7 +3,7 @@ from apikey import apikey
 from langchain.llms import openai
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SimpleSequentialChain
-from userInput import userInput
+from TemplateGenerator import TemplateGenerator
 
 os.environ["OPENAI_API_KEY"] = apikey
 
@@ -12,16 +12,14 @@ DEFAULT = {
 }
 
 class chatBot:
-    def __init__(self, template : PromptTemplate = userInput().getTemplate(), params : dict = DEFAULT):
+    def __init__(self, template : list = TemplateGenerator().getTemplates(), params : dict = DEFAULT):
         self.llm = openai.OpenAI(temperature = params["temperature"])
         self.templates = []
         for t in template:
             self.templates.append(t)
 
-    def generateChain(self):
-        if len(self.templates) == 1:
-            return LLMChain(llm = self.llm, prompt = self.templates[0], verbose = True)
-        chainNeeded = []
+    def generateChains(self) -> list:
+        chains = []
         for i in self.templates:
-            chainNeeded.append(LLMChain(llm = self.llm, prompt = i, verbose = True))
-        return SimpleSequentialChain(chains = i)
+            chains.append((LLMChain(llm = self.llm, prompt = i[0], verbose = True), i[1]))
+        return chains
--- model/main.py
+++ model/main.py
@@ -1,12 +1,14 @@
 import streamlit as strm
 from chatBot import chatBot
 
-CHATBOT = chatBot().generateChain()
+CHATBOTCHAIN = chatBot().generateChains()
 
 strm.title("Travel chatbot")
 prompt = strm.text_input("Pick a location")
 if prompt:
-    resp = CHATBOT.run(location = prompt)
-    strm.write(resp)
+    for chain in CHATBOTCHAIN:
+        c, title = chain
+        strm.write(title)
+        strm.write(c.run(location = prompt))
 
 
