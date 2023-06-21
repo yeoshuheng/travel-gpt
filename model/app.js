416c2a80fef859e81f826f220249dfe6b71862ab
ponghee <tayponghee@gmail.com>
2023-06-21 17:41:16 +0800

    added css,html,js files

---
+++ model/app.js
@@ -0,0 +1,11 @@
+function submitForm() {
+    var city = document.getElementById("cityInput").value;
+    var month = document.getElementById("monthInput").value;
+    var days = document.getElementById("daysInput").value;
+
+    // Do something with the user inputs
+    console.log("City: " + city);
+    console.log("Month: " + month);
+    console.log("Days: " + days);
+}
+
