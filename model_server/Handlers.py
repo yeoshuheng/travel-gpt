a52554918ce2838010dcb91f8739833234a5ef4e
yeoshuheng <yeoshuheng@gmail.com>
2023-06-21 17:46:20 +0800

    feat: setup redis db

--- model_server/Handlers.py
+++ model_server/Handlers.py
@@ -12,3 +12,5 @@ def handleIternary(inpt : tuple, bot : chatBot) -> dict:
       loc, day, month = inpt
       return {"iternary" : bot.generateIternary(loc, day, month)}
 
+def mergeResponse(iter_resp : dict, det_resp : dict) -> dict:
+      return iter_resp | det_resp
--- model_server/main.py
+++ model_server/main.py
@@ -2,24 +2,25 @@ from flask import Flask, request, jsonify
 from jsonrpcserver import dispatch, result
 from ChatBot import chatBot
 from Handlers import *
+from Cache import *
+from db import DB
 
 app = Flask(__name__)
+db = DB()
+CHATBOT = chatBot()
 
 @app.route('/post_json', methods=['POST'])
 def process_json():
     data = request.get_json()
     CHATBOT = chatBot()
-    if data["call_type"] == "1":
-        loc = data["location"]
-        resp = handleDetails(loc, CHATBOT)
-        print(resp)
-    elif data["call_type"] == "0":
-        loc, month, day = data["location"], data["month"], data["day"]
-        resp = handleIternary((loc, day, month), CHATBOT)
-        print(resp)
-    else:
-        return {}
-    return resp
+    loc = data["location"]
+    hit, det_resp = checkCache(loc ,db)
+    if not hit:
+        det_resp = handleDetails(loc, CHATBOT)
+        db.write(loc, det_resp)
+    loc, month, day = data["location"], data["month"], data["day"]
+    iter_resp = handleIternary((loc, day, month), CHATBOT)
+    resp = mergeResponse(iter_resp, det_resp)
     return jsonify(resp)
 
 if __name__ == "__main__":
