cfed0e77331020891316c55e74b0c47a5f8edd1c
pong hee <tayponghee@gmail.com>
2023-06-24 20:21:28 +0800

    merged main to ponghee branch

--- client/app.js
+++ client/app.js
@@ -2,13 +2,18 @@ function submitForm() {
     var city = document.getElementById("cityInput").value;
     var month = document.getElementById("monthInput").value;
     var days = document.getElementById("daysInput").value;
-
+    var jsonData = {c : city, m : month, d : days};
+    var jsonObject = JSON.stringify(jsonData);
+    
     // Display the user inputs
     var userInput = "Location: " + city + ", Month: " + month + ", Days: " + days;
     console.log(userInput);
+    console.log(jsonObject);
 
     // Update the userInput field
     document.getElementById("userInput").value = userInput;
+
+   
 }
 
 
