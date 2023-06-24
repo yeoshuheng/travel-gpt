4fc0b885786efc8fc557e6774a143d1687f6d873
pong hee <tayponghee@gmail.com>
2023-06-24 11:47:42 +0800

    beautified webpage and added an output for user inputs on the page

--- model/app.js
+++ model/app.js
@@ -3,9 +3,12 @@ function submitForm() {
     var month = document.getElementById("monthInput").value;
     var days = document.getElementById("daysInput").value;
 
-    // Do something with the user inputs
-    console.log("City: " + city);
-    console.log("Month: " + month);
-    console.log("Days: " + days);
+    // Display the user inputs
+    var userInput = "Location: " + city + ", Month: " + month + ", Days: " + days;
+    console.log(userInput);
+
+    // Update the userInput field
+    document.getElementById("userInput").value = userInput;
 }
+
 
