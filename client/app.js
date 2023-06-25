9c0f5b936e71c87e3a7cab238e55ea62c47f9999
Parth Gandhi <E0957991@u.nus.edu>
2023-06-25 14:40:04 +0800

    Improved the UI for the website

--- client/app.js
+++ client/app.js
@@ -3,21 +3,26 @@ function submitForm() {
   var months = document.getElementById("monthsInput").value;
   var days = document.getElementById("daysInput").value;
    // Check if any fields are empty
-   if (city === "" || months === "" || (days === ""|| days <= 0) ) {
-      return;
+   if (city === "" || months === "" || days === "") {
+    document.getElementById("errorMessage").innerHTML = "Please fill in all the boxes.";
+    return;
+  }
+
+  if (days <= 0 || days > 14) {
+    document.getElementById("errorMessage").innerHTML = "Please enter a valid number of days (between 1 and 14 days).";
+    return;
   }
   
+  // Clear the error message
+  document.getElementById("errorMessage").innerHTML = "";
   var jsonData = {location : city, month : months, day : days};
   var jsonObject = JSON.stringify(jsonData);
 
+  // Display the "Generating..." message
+  document.getElementById("generating").innerHTML = "Generating...";
+
   // Display the user inputs
   var userInput = "Location: " + city + ", Month: " + months + ", Days: " + days;
-
-  // Clear the form inputs
-  document.getElementById("cityInput").value = "";
-  document.getElementById("monthsInput").value = "";
-  document.getElementById("daysInput").value = "";
-  
   $.ajax({
     type: "POST",
     url: "http://localhost:5000/post_json",
@@ -27,15 +32,22 @@ function submitForm() {
       console.log("POST request successful!");
       const obj = result;
       var output = "";
-      output += "Accommodations: " + obj.accoms + ", ";
-      output += "Cuisine: " + obj.cuisine + ", ";
-      output += "Description: " + obj.desc + ", ";
-      output += "Itinerary: " + obj.iternary + ", ";
-      output += "Transportation: " + obj.transportation + ", ";
-      output += "Weather: " + obj.weather + ", ";
-      output += "Eco-friendliness: " + obj.ecoinfo + ", ";
-      output += "Budget: " + obj.budget;
-      document.getElementById("outputText").value = output;
+      output += "Accommodations: " + "<br>" + obj.accoms  + "<br><br>";
+      output += "Cuisine: " + "<br>" + obj.cuisine  + "<br><br>";
+      output += "Description: " + "<br>" + obj.desc  + "<br><br>";
+      output += "Itinerary: " + "<br>" + obj.iternary  + "<br><br>";
+      output += "Transportation: " + "<br>" + obj.transportation  + "<br><br>";
+      output += "Weather: " + "<br>" + obj.weather  + "<br><br>";
+      output += "Eco-friendliness: " + "<br>" + obj.ecoinfo  + "<br><br>";
+      output += "Budget: " + "<br>" + obj.budget + "<br><br>";
+      document.getElementById("outputText").innerHTML = output;
+      document.getElementById("generating").innerHTML = "";
+
+      // Clear the form inputs
+      document.getElementById("cityInput").value = "";
+      document.getElementById("monthsInput").value = "";
+      document.getElementById("daysInput").value = "";
+      document.getElementById("generating").innerHTML = ""
     },
     error: function (result, status) {
       console.log("POST request failed!");
@@ -43,4 +55,3 @@ function submitForm() {
     }
   });
 }
-
